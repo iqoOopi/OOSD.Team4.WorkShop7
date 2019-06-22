8163dcd1905ccea02907988b9842a454001b638f
tdleslie17 <tdleslie1@gmail.com>
2019-06-22 11:24:06 -0600

    Added Bookings, Products and Suppliers REST Services in case we find them necessary. Tim Leslie.

---
+++ Team4API/src/main/BookingsRESTService.java
@@ -0,0 +1,133 @@
+	/*
+	 ********************************************************************************************
+	 * Course: PROJ 217 Threaded Project
+	 * Purpose: Workshop 7
+	 * Date: June 22, 2019.
+	 * Author: Timothy Leslie
+	 * Description: This is an Bookings REST service to access database 
+	 * information. This service will be called with AJAX from the client-side and used to
+	 * populate an booking object.
+	 *******************************************************************************************/
+package main;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import model.Booking;
+
+@Path("/bookings")
+public class BookingsRESTService {
+
+	@GET
+	@Path("/getallbookings")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllBookings()
+	{
+		//	http://localhost:8080/Team4API/rest/bookings/getallbookings
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		Query query = em.createQuery("select b from Booking b");
+		List<Booking> bookings = query.getResultList();
+		Gson gson = new Gson();
+		Type type = new TypeToken<List<Booking>>() {}.getType();
+		String jsonString = gson.toJson(bookings, type);
+		em.close();
+		return jsonString;
+	}
+	
+	@GET
+	@Path("/getbooking/{ bookingid }")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getBooking(@PathParam("bookingid") int bookingId)
+	{
+		//	http://localhost:8080/Team4API/rest/agents/getagent/5
+		EntityManager em =
+//				Persistence.createEntityManagerFactory("OOSD.Team4.Workshop7.Team4API").createEntityManager();
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+
+		Booking booking = em.find(Booking.class, bookingId);
+		
+		//Query query = em.createQuery("select a from Agent a where a.agentId=" + agentId);
+		//Agent agent = (Agent) query.getSingleResult();
+
+		Gson gson = new Gson();
+		Type type = new TypeToken<Booking>() {}.getType();
+		String jsonString = gson.toJson(booking, type);
+		em.close();
+		return jsonString;
+
+	}
+	
+	@POST
+	@Path("/postbooking")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	public String postBooking(String jsonString)
+	{
+		System.out.println(jsonString);
+		Gson gson = new Gson();
+		Type type = new TypeToken<Booking>() {}.getType();
+
+		Booking booking = gson.fromJson(jsonString, type);
+		EntityManager em =
+//				Persistence.createEntityManagerFactory("OOSD.Team4.Workshop7.Team4API").createEntityManager();
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		em.getTransaction().begin();
+		em.merge(booking);
+		em.getTransaction().commit();
+		return "Booking update completed";
+	}
+
+	@PUT
+	@Path("/putbooking")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	public String putBooking(String jsonString)
+	{
+		Gson gson = new Gson();
+		Type type = new TypeToken<Booking>() {}.getType();
+		String temp = jsonString;
+		Booking booking = gson.fromJson(jsonString, type);
+		EntityManager em =
+//				Persistence.createEntityManagerFactory("OOSD.Team4.Workshop7.Team4API").createEntityManager();
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(booking);
+		em.getTransaction().commit();
+		return "Booking insert completed";
+	}
+	
+	@DELETE
+	@Path("/deletebooking/{ bookingid }")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteBooking(@PathParam("bookingid") int bookingId)
+	{
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		Booking booking = em.find(Booking.class, bookingId);
+		em.getTransaction().begin();
+		em.remove(booking);
+		em.getTransaction().commit();
+		return "Booking deleted";
+	}
+
+}
---
+++ Team4API/src/main/SuppliersRESTService.java
@@ -0,0 +1,130 @@
+/*
+ ********************************************************************************************
+ * Course: PROJ 217 Threaded Project
+ * Purpose: Workshop 7
+ * Date: June 21, 2019.
+ * Author: Timothy Leslie
+ * Description: This is a Suppliers REST service to access database 
+ * information. This service will be called with AJAX from the client-side and used to
+ * populate an supplier object.
+ ********************************************************************************************
+ */
+package main;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+import model.Supplier;
+
+@Path("/suppliers")
+public class SuppliersRESTService {
+
+	@GET
+	@Path("/getallsuppliers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllSuppliers()
+	{
+		//	http://localhost:8080/Team4API/rest/agents/getallproducts
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		Query query = em.createQuery("select s from Supplier s");
+		List<Supplier> suppliers = query.getResultList();
+		Gson gson = new Gson();
+		Type type = new TypeToken<List<Supplier>>() {}.getType();
+		String jsonString = gson.toJson(suppliers, type);
+		em.close();
+		return jsonString;
+	}
+	
+	@GET
+	@Path("/getsupplier/{ supplierid }")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getSupplier(@PathParam("supplierid") int supplierId)
+	{
+		//	http://localhost:8080/Team4API/rest/products/getproduct/5
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+
+		Supplier supplier = em.find(Supplier.class, supplierId);
+		
+		Gson gson = new Gson();
+		Type type = new TypeToken<Supplier>() {}.getType();
+		String jsonString = gson.toJson(supplier, type);
+		em.close();
+		return jsonString;
+
+	}
+	
+	@POST
+	@Path("/postsupplier")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	public String postSupplier(String jsonString)
+	{
+		System.out.println(jsonString);
+		Gson gson = new Gson();
+		Type type = new TypeToken<Supplier>() {}.getType();
+
+		Supplier supplier = gson.fromJson(jsonString, type);
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		em.getTransaction().begin();
+		em.merge(supplier);
+		em.getTransaction().commit();
+		return "Supplier update completed";
+	}
+	
+	
+	@PUT
+	@Path("/putsupplier")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces(MediaType.TEXT_PLAIN)
+	public String putSupplier(String jsonString)
+	{
+		Gson gson = new Gson();
+		Type type = new TypeToken<Supplier>() {}.getType();
+		String temp = jsonString;
+		Supplier supplier = gson.fromJson(jsonString, type);
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(supplier);
+		em.getTransaction().commit();
+		return "Supplier insert completed";
+	}
+	
+	@DELETE
+	@Path("/deletesupplier/{ supplierid }")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteSupplier(@PathParam("supplierid") int supplierId)
+	{
+		EntityManager em =
+				Persistence.createEntityManagerFactory("Team4API").createEntityManager();
+		
+		Supplier supplier = em.find(Supplier.class, supplierId);
+		em.getTransaction().begin();
+		em.remove(supplier);
+		em.getTransaction().commit();
+		return "Supplier deleted";
+	}
+
+}
