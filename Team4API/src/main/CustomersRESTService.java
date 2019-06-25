562e99a641785c10eb48cd7c955302bf8a9ffd1b
Et135 <eric.truong.135@gmail.com>
2019-06-25 02:15:38 -0600

    add methods to customersRESTService

--- Team4API/src/main/CustomersRESTService.java
+++ Team4API/src/main/CustomersRESTService.java
@@ -154,5 +154,335 @@ public class CustomersRESTService {
 		em.getTransaction().commit();
 		return "Customer deleted";
 	}
+	
+	
+	/*Eric Addtional Codes Starts*/
+	private class LoginCredentials{
+
+		private String CustEmail;
+		private String CustPassword;
+		
+		public LoginCredentials(){
+		}
+
+		public String getCustEmail() {
+			return CustEmail;
+		}
+
+		public void setCustEmail(String custEmail) {
+			this.CustEmail = custEmail;
+		}
+
+		public String getCustPassword() {
+			return CustPassword;
+		}
+
+		public void setCustPassword(String custPassword) {
+			this.CustPassword = custPassword;
+		}
+	}
+	
+	private enum letterNotation {A,B,C,D,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,U,V,W,X,Y,Z}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+	public Response postLogin(String request) {
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		try{
+			///
+			// Request comes in as Base64 encoded
+			///
+			request=new String(Base64.decodeBase64(request));
+			Gson gson = new Gson();
+			LoginCredentials jsonIN = gson.fromJson(request,LoginCredentials.class);
+		
+			String query = "SELECT c FROM Customer c WHERE c.custEmail=:requestEMAIL";
+			Query q = em.createQuery(query);
+			q.setParameter("requestEMAIL", jsonIN.getCustEmail());
+			
+			String jsonOUT = gson.toJson(q.getSingleResult());
+			Customer customer = gson.fromJson(jsonOUT, Customer.class);
+			
+			String passwordIN = jsonIN.getCustPassword();
+			
+			if(passwordIN.equals(customer.getCustPassword())){
+				///
+				// SUCCESSFUL LOGIN
+				// TOKEN GENERATION
+				///
+				String RANDOMTIME = String.valueOf(Calendar.getInstance().getTimeInMillis());
+				RANDOMTIME = RANDOMTIME.substring(RANDOMTIME.length()-3,RANDOMTIME.length());
+				StringBuilder sb = new StringBuilder("");
+				sb.append(RANDOMTIME);
+				sb.append(customer.getCustEmail());
+				sb.append(RANDOMTIME);
+				String strTOKEN = sb.toString();
+				byte[] byteTOKEN = Base64.encodeBase64(strTOKEN.getBytes());
+				String TOKEN = new String(byteTOKEN);
+				
+				///
+				// IF TOKEN EXISTS, UPDATE IT
+				///
+				String selectQuery = "SELECT c FROM Customerstoken c WHERE c.customerId=:CUSTOMERID";
+				Query q1 = em.createQuery(selectQuery);
+				q1.setParameter("CUSTOMERID", customer.getCustomerId());
+				if(q1.getResultList().size()>0){
+					em.getTransaction().begin();
+					String deleteQuery = "DELETE FROM customerstokens WHERE customerId = ?";
+					Query q2 = em.createNativeQuery(deleteQuery);
+					q2.setParameter(1, customer.getCustomerId());
+					q2.executeUpdate();
+					em.getTransaction().commit();
+				}
+				///
+				// Hand out new token
+				///
+				em.getTransaction().begin();
+				String insertQuery = "INSERT INTO customerstokens (CustomerId,TOKEN,TOKENCREATED) VALUES (?,?,?)";
+				Query q3 = em.createNativeQuery(insertQuery);
+				q3.setParameter(1, customer.getCustomerId());
+				q3.setParameter(2, TOKEN);
+				q3.setParameter(3, Calendar.getInstance().getTime());
+				q3.executeUpdate();
+				em.getTransaction().commit();
+				em.close();
+				emfactory.close();
+				///
+				// FINALLY RETURN
+				///
+				
+				return Response.ok(gson.toJson(customer),MediaType.APPLICATION_JSON).header("AUTHTOKEN", TOKEN).build();
+			}else{
+				return Response.status(Status.UNAUTHORIZED).build();
+			}
+			
+		}catch(NoResultException e){
+			return Response.status(Status.UNAUTHORIZED).build();
+		}
+	}
+	
+	@POST
+	@Path("/assignagent2cust")
+	@Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+	public Response postAssignAgent(String custJSON){
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		Type type = new TypeToken<Customer>(){}.getType();
+		Customer c = new Gson().fromJson(custJSON, type);
+		
+		em.getTransaction().begin();
+		Query q = em.createNativeQuery("UPDATE customers SET AgentId = ? WHERE CustomerId = ?");
+		int agentid = GenerateRandomAgent();
+		q.setParameter(1, agentid);
+		q.setParameter(2, c.getCustomerId());
+		
+		if(q.executeUpdate()==1){
+			em.getTransaction().commit();
+			em.close();
+			emfactory.close();
+			return Response.status(Status.OK).header("AGENTID", agentid).build();
+		}else{
+			em.close();
+			emfactory.close();
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+	}
+	
+	private int GenerateRandomAgent(){
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		Query q = em.createQuery("SELECT a FROM Agent a");
+		
+		List<Agent> agents = q.getResultList();
+		ArrayList<Integer> ids = new ArrayList<Integer>();
+		
+		for(int i=0;i<agents.size();i++){
+			ids.add(agents.get(i).getAgentId());
+		}
+		
+		Random random = new Random();
+		
+		return ids.get(random.nextInt(ids.size()));
+	}
+	
+	
+	@GET
+	@Path("/get_my_bookings/{custid}")
+    @Produces(MediaType.APPLICATION_JSON)
+	public String getMyBookingWPackage(@PathParam("custid") int custid) {
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		Query qBookings = em.createQuery("SELECT b FROM Booking b WHERE b.customerId=:CUSTID");
+		qBookings.setParameter("CUSTID", custid);
+		List<Booking> bookings = qBookings.getResultList();
+		
+		Query qPackage;
+		List<Package> packages__bookings = new ArrayList<Package>();
+		for(int i=0;i<bookings.size();i++){
+			qPackage = em.createQuery("SELECT p FROM Package p WHERE p.packageId=:PACKAGEID");
+			qPackage.setParameter("PACKAGEID", bookings.get(i).getPackageId());
+			packages__bookings.add((Package)qPackage.getSingleResult());
+		}
+		
+		List<BookingWPackage> bookingsWpackage = new ArrayList<BookingWPackage>();
+		BookingWPackage bookingWpackage;
+		for(int i=0;i<bookings.size();i++){
+			bookingWpackage = new BookingWPackage();
+			bookingWpackage.setBookingId(bookings.get(i).getBookingId());
+			bookingWpackage.setBookingDate(bookings.get(i).getBookingDate());
+			bookingWpackage.setBookingNo(bookings.get(i).getBookingNo());
+			bookingWpackage.setCustomerId(bookings.get(i).getCustomerId());
+			bookingWpackage.setTravelerCount(bookings.get(i).getTravelerCount());
+			bookingWpackage.setTripTypeId(bookings.get(i).getTripTypeId());
+			bookingWpackage.setPackageId(bookings.get(i).getPackageId());
+			bookingWpackage.setPkgName(packages__bookings.get(i).getPkgName());
+			bookingWpackage.setPkgStartDate(packages__bookings.get(i).getPkgStartDate());
+			bookingWpackage.setPkgEndDate(packages__bookings.get(i).getPkgEndDate());
+			bookingWpackage.setPkgDesc(packages__bookings.get(i).getPkgDesc());
+			bookingWpackage.setPkgBasePrice(packages__bookings.get(i).getPkgBasePrice());
+			bookingWpackage.setPkgAgencyCommission(packages__bookings.get(i).getPkgAgencyCommission());
+			
+			bookingsWpackage.add(bookingWpackage);
+		}
+		
+		String json = new Gson().toJson(bookingsWpackage);
+		
+		em.close();
+		emfactory.close();			
+		return json;
+	}
+	
+	
+	@POST
+	@Path("/create_booking")
+    @Produces(MediaType.TEXT_PLAIN)
+	public Response postBooking(String request) {
+		
+		//to convert JSON data to entity		
+		
+		Type type = new TypeToken<Booking>(){}.getType();
+		Booking b = new Gson().fromJson(request, type);
+		String bookingNo = generateBookingNo();
+		b.setBookingNo(bookingNo);
+		
+		// pass the entity to persistence framework
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		//won't work without transaction, throws exception
+		em.getTransaction().begin();
+		em.persist(b);
+		em.getTransaction().commit();
+		
+		return Response.ok().build(); 	
+	}
+
+	private String generateBookingNo(){
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		Query q = em.createQuery("SELECT b FROM Booking b");
+		
+		List<Booking> bookings = q.getResultList();
+		
+		List<String> bookingNos = new ArrayList<String>();
+		for(int i=0;i<bookings.size();i++){
+			bookingNos.add(bookings.get(i).getBookingNo());
+		}
+		
+		String bookingno = "";
+		boolean keeplooking = true;
+		Random random = new Random();
+		while(keeplooking){
+			keeplooking=false;
+			bookingno = "";
+			for(int i=0;i<4;i++){
+				bookingno+=letterNotation.values()[random.nextInt(24)];
+			}
+			for(int i=0;i<2;i++){
+				bookingno+=random.nextInt(10);
+			}
+			for(int i=0;i<bookingNos.size();i++){
+				if(bookingno==bookingNos.get(i)){
+					keeplooking=true;
+				}
+			}
+		}
+		
+		em.close();
+		emfactory.close();
+		return bookingno;
+	}
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response postUpdateAccount(String custJSON){
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		Type type = new TypeToken<Customer>(){}.getType();
+		Customer c = new Gson().fromJson(custJSON, type);
+		
+		em.getTransaction().begin();
+		Query q = em.createNativeQuery("UPDATE customers SET "+
+		"CustFirstName = ?,CustLastName = ?,CustAddress = ?,CustCity = ?,CustProv = ?,CustPostal = ?,CustCountry = ?,CustHomePhone = ?,CustBusPhone = ?,CustEmail = ? "+
+				"WHERE CustomerId = ?");
+		q.setParameter(1, c.getCustFirstName());
+		q.setParameter(2, c.getCustLastName());
+		q.setParameter(3, c.getCustAddress());
+		q.setParameter(4, c.getCustCity());
+		q.setParameter(5, c.getCustProv());
+		q.setParameter(6, c.getCustPostal());
+		q.setParameter(7, c.getCustCountry());
+		q.setParameter(8, c.getCustHomePhone());
+		q.setParameter(9, c.getCustBusPhone());
+		q.setParameter(10, c.getCustEmail());
+		q.setParameter(11, c.getCustomerId());
+		
+		if(q.executeUpdate()==1){
+			em.getTransaction().commit();
+			em.close();
+			emfactory.close();
+			return Response.status(Status.OK).build();
+		}else{
+			em.close();
+			emfactory.close();
+			return Response.status(Status.BAD_REQUEST).build();
+		}
+	}
+	
+	@POST
+	@Path("/register_customer")
+    @Produces(MediaType.TEXT_PLAIN)
+	public Response postCustomer(String request) {
+		///
+		// Customer is passed to service as JSON
+		///
+		Type type = new TypeToken<Customer>(){}.getType();
+		Customer c= new Gson().fromJson(request, type);
+		
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Team4API");
+		EntityManager em = emfactory.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(c);
+		em.getTransaction().commit();
+		
+		return Response.status(Status.OK).build();
+	}
+	
 
 }
